0ffa166a1734a5b2303a6605f41ad1543844a96c
virhi <vincent.rhino@gmail.com>
2014-10-14 17:51:27 +0200

    refactoring list promise

--- modules/ui/controler.js
+++ modules/ui/controler.js
@@ -12,6 +12,96 @@ module.exports = function(app, settings, callback) {
     app.set('view engine', 'twig');
     app.engine('twig', twig.__express);
 
+    var getListPage = function (page, listFilter) {
+
+        var list = {};
+        list['objectName'] = listFilter.type;
+        list['settings']   = settings;
+
+        objectService.getStructurePromise(settings).then(function(structure) {
+            list['structure'] = structure;
+
+            return structure;
+
+        }, function(value) {
+            page.res.render('test', {
+                page: 'sa a echoué 1',
+                list: list
+            });
+        }).then(function(structure) {
+
+            objectService.getMenuP(structure, listFilter.type).then(function(menu) {
+                list['menu'] = menu;
+
+                objectService.getListObject(settings, listFilter).then(function(listObject) {
+                    list['listObject'] = listObject;
+                    objectService.getCollectionsPromise(settings, list.structure, listFilter.type).then(function(collection) {
+                        list['collection'] = collection;
+
+                        objectService.getEmbedFieldsPromise(settings, collection, structure).then(function(embedFields) {
+                            list['embedFields'] = embedFields;
+
+                            objectService.getFieldsPromise(settings, structure, listFilter.type).then(function(fields) {
+                                list['fields'] = fields;
+                                page.res.render('test', {
+                                    page: 'yooo fields 1',
+                                    list: list
+                                });
+                            }, function(error){
+                                page.res.render('test', {
+                                    page: 'fields error 1',
+                                    list: list
+                                });
+                            });
+
+
+
+                        }, function(error) {
+                            page.res.render('test', {
+                                page: 'embedFields error 1',
+                                list: list
+                            });
+                        });
+                    }, function(error){
+
+                        page.res.render('test', {
+                            page: 'collection error 1',
+                            list: list
+                        });
+                    });
+
+                },function(error) {
+                    console.log(error);
+                    page.res.render('test', {
+                        page: 'erreur list 1',
+                        list: list
+                    });
+                });
+
+            }, function(error){
+                page.res.render('test', {
+                    page: 'erreur menu 1',
+                    list: list
+                });
+            })
+
+        }).catch(function(error) {
+            console.log('exption');
+        });
+    };
+
+    app.get('/test/:object', function(req, res) {
+
+        var listFilter = {
+            type : req.params.object
+        }
+
+        var page = {
+            req : req,
+            res: res
+        }
+        getListPage(page, listFilter);
+    });
 
     app.get('/', function(req, res){
 
--- modules/ui/objectService.js
+++ modules/ui/objectService.js
@@ -1,7 +1,137 @@
 var extend  = require('util')._extend;
 var request = require('request');
+var RSVP    = require('rsvp');
 
 var objectService = {
+
+
+
+    getMenuP : function(structure, current) {
+        var promise = new RSVP.Promise(function(resolve, reject) {
+            var menu = [];
+
+            for (x in structure) {
+                var isCurrant = false;
+
+                if (x == current) {
+                    isCurrant = true;
+                }
+
+                var tmpObj = {
+                    name: structure[x].name,
+                    url: '/list/' + structure[x].name,
+                    current: isCurrant
+                }
+                menu.push(tmpObj);
+            }
+            resolve(menu);
+        });
+        return promise;
+    },
+
+    getStructurePromise: function(settings) {
+
+
+        var promise = new RSVP.Promise(function(resolve, reject) {
+            var reqOptions = {
+                url: settings.api.host + '/objects/all',
+                headers: {
+                    'User-Agent': 'request'
+                }
+            };
+
+            request(reqOptions, function callback(error, response, body) {
+
+                if (error) {
+                    throw error;
+                } else {
+                    switch (response.statusCode) {
+                        case 200:
+                        case 201:
+
+                            var result   = {};
+                            var tmpStruc = JSON.parse(body);
+                            for (x in tmpStruc) {
+                                result[tmpStruc[x].name] = tmpStruc[x];
+                            }
+                            resolve(result);
+                            break;
+                        case 404:
+                            reject('404');
+                            break;
+                        case 500:
+                            reject('500');
+                            break;
+                    }
+                }
+            });
+        });
+
+        return promise;
+    },
+
+
+    getListObject : function(settings, listFilter) {
+        var promise = new RSVP.Promise(function(resolve, reject) {
+
+            var reqOptions = {
+                url: settings.api.host + '/' + listFilter.type,
+                headers: {
+                    'User-Agent': 'request'
+                }
+            };
+
+            request(reqOptions, function callback(error, response, body) {
+
+                if (error) {
+                    throw error;
+                } else {
+                    switch (response.statusCode) {
+                        case 200:
+                        case 201:
+                            resolve(JSON.parse(body));
+                            break;
+                        case 404:
+                            reject('404');
+                            break;
+                        case 500:
+                            reject('500');
+                            break;
+                    }
+                }
+            });
+
+        });
+        return promise;
+    },
+
+
+    getJson : function(reqOptions) {
+
+        var promise = new RSVP.Promise(function(resolve, reject) {
+
+            request(reqOptions, function callback(error, response, body) {
+
+                if (error) {
+                    throw error;
+                } else {
+                    switch (response.statusCode) {
+                        case 200:
+                        case 201:
+                            resolve(JSON.parse(body));
+                            break;
+                        case 404:
+                            reject('404');
+                            break;
+                        case 500:
+                            reject('500');
+                            break;
+                    }
+                }
+            });
+        });
+        return promise;
+    },
 
     getEmbedFields: function (settings, sendObject) {
         var collections = sendObject.collections;
@@ -35,6 +165,84 @@ var objectService = {
 
     },
 
+    getEmbedFieldsPromise: function (settings, collections, models) {
+
+
+        var promise = new RSVP.Promise(function(resolve, reject) {
+
+            var tmpEmbedFields = [];
+
+            var targetObjectsNameTmp = '';
+
+            if (Array.isArray(collections)) {
+                for (collectionsCompteur = 0; collectionsCompteur < collections.length; collectionsCompteur++) {
+                    var targetObjectsNameTmp =  collections[collectionsCompteur].targetObject;
+
+                    for (x in models) {
+                        if (x == targetObjectsNameTmp) {
+                            var tmpListField = models[x].model.fields;
+                            for (field in tmpListField) {
+                                if (tmpListField[field].hasOwnProperty('key') && tmpListField[field].key == true) {
+                                    var tpmObject = {
+                                        name: collections[collectionsCompteur].fieldName,
+                                        field: tmpListField[field].name
+                                    }
+                                    tmpEmbedFields.push(tpmObject);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            resolve(tmpEmbedFields);
+        });
+        return promise;
+    },
+
+    /* get fields for object description */
+    getCollectionsPromise: function(settings, structure, objectName) {
+
+        var promise = new RSVP.Promise(function(resolve, reject) {
+            var squelette  = structure[objectName];
+            var result     = null;
+            var tmpFields  = squelette;
+            var fieldsPath = settings.squeleteHasmany.split('.');
+
+            function getIndex(field, index) {
+                if (tmpFields.hasOwnProperty(field)) {
+                    tmpFields  = tmpFields[field];
+                    return true;
+                } else {
+                    tmpFields = null;
+                    return false;
+                }
+            }
+
+            fieldsPath.every(getIndex);
+
+            result = tmpFields;
+            resolve(result);
+        });
+
+        return promise;
+
+    },
+
+
+    buildMenu: function(structure) {
+        var menu = [];
+        function buildMenu(field, index) {
+            var tmpObj = {
+                name: field.name,
+                url: '/list/' + field.name
+            }
+            menu[index] = tmpObj;
+        };
+        structure.forEach(buildMenu);
+
+        return menu;
+    },
+
     /* get fields for object description */
     getCollections: function(settings, squelette) {
         var result     = null;
@@ -55,6 +263,34 @@ var objectService = {
 
         result = tmpFields;
         return result;
+    },
+
+    /* get fields for object description */
+    getFieldsPromise: function(settings, structure, objectName) {
+
+        var promise = new RSVP.Promise(function(resolve, reject) {
+            var squelette  = structure[objectName];
+            var result     = null;
+            var tmpFields  = squelette;
+            var fieldsPath = settings.squeleteFields.split('.');
+
+            function getIndex(field, index) {
+                if (tmpFields.hasOwnProperty(field)) {
+                    tmpFields  = tmpFields[field];
+                    return true;
+                } else {
+                    tmpFields = null;
+                    return false;
+                }
+            }
+
+            fieldsPath.every(getIndex);
+
+            result = tmpFields;
+            resolve(result);
+        });
+
+        return promise;
     },
 
     /* get fields for object description */
